ac54b02f732f704e3e92db91ec05ebb7caff8a92
jess <jessachandler@gmail.com>
2025-03-28 19:51:43 -0700

    rearranges functions

--- functions/bike_parking_functions.R
+++ functions/bike_parking_functions.R
@@ -66,3 +66,30 @@ update_bikeparking_with_new <- function(name, address, desc = NA,
   new_bikeparking <- add_new(new_row = new_row)
   save_bike_parking_data(new_bikeparking)
 }
+
+
+
+get_bike_parking_path <- function(local = TRUE) {
+  if(local) {
+    return("./kitsap-bike-parking.csv")
+  } else {
+    return("https://raw.githubusercontent.com/monkeywithacupcake/kitsap-bike-parking/main/kitsap-bike-parking.csv")
+  }
+}
+
+get_rack_image_path <- function(local = TRUE) {
+  if(local) {
+    return("./img/racks/")
+  } else {
+    return("https://raw.githubusercontent.com/monkeywithacupcake/kitsap-bike-parking/main/img/racks/")
+  }
+}
+
+get_bike_parking_data <- function(local = TRUE) {
+  dpath <- get_bike_parking_path(local)
+  readr::read_csv(dpath)
+}
+
+save_bike_parking_data <- function(bikeparking){
+  readr::write_csv(bikeparking, file="./kitsap-bike-parking.csv")
+}
