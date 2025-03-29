510a95cdd25a269de0f048294051ddfc9f08e67e
jess <jessachandler@gmail.com>
2025-03-28 19:52:35 -0700

    uses new functions

--- addbikeparking.R
+++ addbikeparking.R
@@ -1,61 +1,20 @@
 
 # allows us to add a new bike parking spot
+source("./functions/bike_parking_functions.R")
 
-library(tidyverse)
-library(mapboxapi)
-
-
-newaddress <- "731 Bethel Ave, Port Orchard, WA 98366"
-newname <-"BETHEL GRAVITY COFFEE"
-newimage <- "Bethel Gravity Coffee.jpg" # NA if none
-newdesc <- "On the south side"
-newvariant <- "WAVE" #VARIANT GRID WAVE BOLLARD STAPLE CUSTOM
-newcovered <- "NO" #YESNO
-newspaces <- 2 # number
-
-# read it in
-bikeparking <- read_csv("./kitsap-bike-parking.csv") 
-
-# add the stuff
-bikeparking <- bikeparking %>%
-  add_row(NAME = newname,
-          ADDRESS = newaddress,
-          DESC = newdesc,
-          VARIANT = newvariant,
-          COVERED = newcovered,
-          SPACES = newspaces,
-          IMG = newimage,
-          LNG = mb_geocode(newaddress)[1],
-          LAT = mb_geocode(newaddress)[2]
-  )
-
-write_csv(bikeparking, file="./kitsap-bike-parking.csv")
-
-
-bikeparking %>%
-  group_by(COVERED) %>%
-  summarize(SPACES = sum(SPACES))
-
-# mapbox_map <- leaflet() %>%
-#   addMapboxTiles(style_id = "light-v9",
-#                  username = "mapbox") %>%
-#   setView(lng = -122.6413,
-#           lat = 47.6477,
-#           zoom = 11)
-# 
-# mapbox_map %>% 
-#   leaflet() %>%
-#   setView(lng = -122.6413,
-#           lat = 47.6477,
-#           zoom = 11)%>%
-#   addTiles() %>%
-#   addCircleMarkers(data = bikeparking, lng = ~LNG, lat = ~LAT, color = "#301934",
-#                    label = ~NAME, radius = 7,  fillOpacity = 0.7, fillColor = "#D8BFD8") 
-#markerOptions(interactive = FALSE))
-#labelOptions(noHide = TRUE, permanent = TRUE))
-
-
-
+the_new_row <- create_new(
+  name = 'fake',
+  address = "731 Bethel Ave, Port Orchard, WA 98366",
+  image = "Bethel Gravity Coffee.jpg",
+  desc = "On the south side",
+  spaces = 4,
+  covered = "NO",
+  variant = "WAVE"
+)
+new_row <- create_new(name, address, desc,variant,covered,spaces,image)
+new_bikeparking <- add_new(new_row = new_row)
+stop() # make sure that you are satisfied before saving
+save_bike_parking_data(new_bikeparking)
 
 
 
